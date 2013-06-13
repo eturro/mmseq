a52f0486ac2b2fd5464145891b985754b44760d2
Ernest Turro <et341@cam.ac.uk>
2013-06-13 22:19:51 +0100

    Added mmseq.R, including polyclass function

--- doc/countsDE.md
+++ doc/countsDE.md
@@ -1,6 +1,6 @@
 #### Feeding MMSEQ estimated counts to DESeq or edgeR
 
-The MMSEQ expression estimates are roughly in FPKM units (fragments per kilobase of transcript per million mapped reads or read pairs), which makes different samples broadly comparable. You may read in the output from multiple samples using the `readmmseq.R` R script included in the `misc` directory (requires R &ge; 2.9). The `readmmseq` function takes five optional arguments:
+The MMSEQ expression estimates are roughly in FPKM units (fragments per kilobase of transcript per million mapped reads or read pairs), which makes different samples broadly comparable. You may read in the output from multiple samples using the `mmseq.R` R script included in the `misc` directory (requires R &ge; 2.9). The `readmmseq` function defined in `mmseq.R` takes five optional arguments:
 
 -  `mmseq_files`: vector of `.mmseq` files to read in
 -  `sample_names`: vector of strings containing the sample names
@@ -12,7 +12,7 @@ The function returns a list in which columns have been collated from the MMSEQ f
 
 To test for differential expression with [edgeR](http://dx.doi.org/10.1186/gb-2010-11-3-r25) or [DESeq](http://dx.doi.org/10.1186/gb-2010-11-10-r106) instead of using [mmdiff](https://github.com/eturro/mmseq#differential-expression-analysis), the estimated counts need to be used. E.g., to test for DE between two groups of two samples, run the following code in R from the directory containing the mmseq output files:
 
-    source("/path/to/readmmseq.R")
+    source("/path/to/mmseq.R")
     library(edgeR)
     library(DESeq) # version >=1.5
 
---
+++ misc/mmseq.R
@@ -0,0 +1,262 @@
+readmmseq <- function( mmseq_files = grep("gene|identical", dir( pattern="\\.mmseq$" ), value=TRUE, invert=TRUE),
+		 sample_names = sub("\\.mmseq","", mmseq_files), normalize=TRUE, partition="", common_set=NULL) {
+	
+	cat( "Using mmseq files:\n", paste("\t", mmseq_files, sep="", collapse="\n"), "\n") 
+	cat( "Using sample names:\n", paste("\t", sample_names, sep="", collapse="\n"), "\n") 
+	if( length(mmseq_files) == 0 || length(sample_names)  != length(mmseq_files) ) {
+		cat( "ERROR:\n\tplease define mmseq files and sample names for each file.\n" )
+		return(NULL)
+	}
+	
+	# read in mmseq data
+	mu = c()
+	sd = c()
+	mcse = c()
+  iact = c()
+	effective_lengths = c()
+	true_lengths = c()
+  n_reads <- c()
+  unique_hits <- c()
+  ntranscripts<- c()
+  observed<- c()
+  dup_sample_names <- c()
+	cat( "Reading mmseq output files\n" )
+	for( i in 1:length(mmseq_files) ) {
+		dat = read.table( mmseq_files[i], header=TRUE, stringsAsFactors=FALSE, sep="\t")
+    if(! any(colnames(dat)=="log_mu") ||
+       ! any(colnames(dat)=="sd") || 
+       ! any(colnames(dat)=="mcse") || 
+       ! any(colnames(dat)=="iact") || 
+       ! any(colnames(dat)=="unique_hits") ||
+       ! any(colnames(dat)=="ntranscripts") ||
+       ! any(colnames(dat)=="observed") ||
+       ! any(colnames(dat)=="effective_length") ||
+       ! any(colnames(dat)=="true_length")) {
+      stop("Colnames must contain log_mu, sd, mcse, iact, effective_length, true_length, ntranscripts, observed and unique_hits.")
+    }
+    spl = strsplit(partition, "|", fixed=TRUE)[[1]]
+    if(length(spl)==0) spl=".*"
+    if(length(spl)>0 && any(spl=="")) {
+      stop("Cannot split using empty string.")
+    }
+    x=grep(spl[1], dat[,grepl("id", colnames(dat))]) 
+    if(length(x)==0) spl=".*" # not a sample to partition
+
+    for(p in spl) {
+      cat("\t", mmseq_files[i], "(",p, ")","\t",sep="")
+      dup_sample_names <- c(dup_sample_names, sample_names[i])
+      if(p != ".*") dup_sample_names[length(dup_sample_names)] = paste(dup_sample_names[length(dup_sample_names)], p,sep="")
+
+      x=grepl(p, dat[,grepl("id", colnames(dat))])
+      if(!is.null(common_set)) {
+        if(p != ".*") {
+        #  x = x & sub(p, "", dat[,grepl("id", colnames(dat))]) %in% common_set
+          x = match(common_set, sub(p, "", dat[, grepl("id", colnames(dat))]))
+        } else {
+        #  x = x & dat[,grepl("id", colnames(dat))] %in% common_set
+          x = match(common_set, dat[,grepl("id", colnames(dat))])
+        }
+      }
+
+      mu <- cbind(mu, dat$log_mu[x])
+      sd <- cbind(sd, dat$sd[x])
+      mcse <- cbind(mcse, dat$mcse[x])
+      iact<- cbind(iact, dat$iact[x])
+      unique_hits <- cbind(unique_hits, dat$unique_hits[x])
+      ntranscripts<- cbind(ntranscripts, dat$ntranscripts[x])
+      observed <- cbind(observed, dat$observed[x])
+      effective_lengths <- cbind(effective_lengths, dat$effective_length[x])
+      true_lengths <- cbind(true_lengths, dat$true_length[x])
+      if(i==1) {
+        rownames(mu) = rownames(sd) = rownames(mcse)= rownames(iact) = rownames(unique_hits)=rownames(ntranscripts)=rownames(observed)=rownames(true_lengths)=rownames(effective_lengths)=
+          sub(partition, "", dat[x,grepl("id", colnames(dat))])
+      } else {
+        if(! all(rownames(mu) == sub(partition, "", dat[x,grepl("id", colnames(dat))]) )) {
+
+          stop("Attempting to join incompatible mmseq files (different features).\n")
+        }
+      }
+      ta <- readLines(mmseq_files[i], n=100) # header should be no more than 100 lines
+      ta <- strsplit(ta[grep("# Mapped fragments", ta)], " ")[[1]]
+      n_reads <- c(n_reads, as.numeric(ta[length(ta)]))
+      cat( "found", n_reads[length(n_reads)], "aligned reads (or read pairs)\n" )
+    }
+  }
+  colnames(mu)=colnames(sd)=colnames(mcse)=colnames(iact)=colnames(unique_hits)=colnames(ntranscripts)=colnames(observed)=colnames(effective_lengths)=colnames(true_lengths)=dup_sample_names
+  names(n_reads) <- dup_sample_names
+
+  if(!all(is.na(true_lengths)) && any(apply(true_lengths,1, function(x) { !all(x==x[1])}))) {
+    warning("True transcript lengths differ across files.\n")
+  }
+
+  # un-log the estimates 
+  cat( "Unlogging and unstandardising the estimates\n" )
+  counts = exp(1)^(mu)
+  
+  # multiply by library size
+  counts= sapply( colnames(counts), function(x) { counts[,x]*n_reads[x] })
+  rownames(counts)=rownames(mu)
+
+  # multiply by transcript length and divide by 10^9
+  counts = sapply( colnames(mu), function(x) { counts[,x] = (counts[,x]*effective_lengths[,x])/(1e3*1e6) } )
+  counts[is.na(counts)] = 0 # the ones that had no expression in MMSEQ
+	
+  if(normalize && ncol(mu)>1) {
+    cat("Normalizing expression estimates with log factors:\n")
+    minfrac = max(0.2, (ncol(mu) - floor(ncol(mu)^2/160))/ncol(mu))
+    cat("Min frac: ", minfrac, "\n")
+    uh = apply(unique_hits,1,function(x){sum(x>=1) >= minfrac*length(x) })
+    sf <- apply(mu[uh,] - rowMeans(mu[uh,], na.rm=TRUE) ,2, function(x){median(x[is.finite(x)])})
+    for(i in 1:length(sf)) {
+      mu[,i] <- mu[,i] - sf[i]
+      cat("\t", dup_sample_names[i],"\t", signif(sf[i],4),"\n")
+    }
+    cat("Normalizing counts with factors:\n")
+    sf <- apply(log(counts[uh,]) - rowMeans(log(counts[uh,]), na.rm=TRUE), 2, function(x){median(x[is.finite(x)])})
+    for(i in 1:length(sf)) {
+      counts[,i] <- counts[,i]/exp(sf)[i]
+      cat("\t", dup_sample_names[i],"\t", signif(exp(sf[i]),4),"\n")
+    }
+  }
+  
+  return(list(log_mu=mu, sd=sd, mcse=mcse, iact=iact, effective_length=effective_lengths, true_length=true_lengths, unique_hits=unique_hits, ntranscripts=ntranscripts[,1], observed=observed, counts=counts))
+}
+
+
+ll.func <- function(pars, x=x,e=e, M=M, P=P) {
+  N=length(x)
+  C=ncol(P)
+  V=ncol(M)
+  par.alpha=pars[1]
+  par.beta=pars[2:(V+1)]
+  if(C==1) {
+    par.gamma <- matrix(0,nrow=1,ncol=1)
+    par.sigma2 <- matrix(pars[length(pars)],ncol=1)
+  } else {
+    par.gamma <- matrix(c(0,pars[(V+2):(V+C)]), ncol=1)
+    par.sigma2 <- matrix(pars[(V+C+1):length(pars)],ncol=1)
+  }
+  Lambda = diag(1/as.vector(P %*% par.sigma2 + e))
+  y=(x - rep(par.alpha,N) - M %*% par.beta - P %*% par.gamma) 
+  return(-( rep(1,N) %*% diag(log(1/diag(Lambda))) %*% rep(1,N) + 
+    t(y) %*% Lambda %*% (y)
+  ))
+}
+
+
+polyclass <- function(files, prior=NULL) {
+  if(length(files)<2) {
+    stop("Specify at least two mmdiff files")
+  }
+  n=length(files)+1
+  if(length(prior) != n || sum(prior) != 1) {
+    stop("Specify a vector of ", n, " prior probabilities adding up to 1")
+  }
+  print("Note: make sure model 0 is the same in each comparison (only model 1 should differ)")
+  if(is.null(prior)) {
+    warning("Assuming flat prior across models")
+    priorm <- rep(1/n,n)
+  } 
+  res <- read.table(files[1],header=1)
+  postm <- matrix(nrow=nrow(res), ncol=n)
+  colnames(postm) <- paste("postprob_model",0:(n-1),sep="")
+  bf <- matrix(nrow=nrow(res), ncol=n) # relative to model 1
+  bf[,1] <- 1
+  for(j in 1:length(files)) {
+    bf[,j+1] <- read.table(files[j],header=1)$bayes_factor
+  }
+
+  for(i in 1:nrow(res)) {
+    summ <- sum(bf[i,]*priorm)
+    postm[i,] <- bf[i,]*priorm/summ
+    if(sum(is.infinite(bf[i,]))>1) {
+      warning(paste("More than one BF inf", i))
+    } else if(sum(bf[1,]==0)>1) {
+      warning(paste("More than one BF zero", i))
+    } else if(sum(is.infinite(bf[i,]))==1) {
+      postm[i,is.infinite(bf[i,])] <- 1
+    }
+  }
+  w=grep("posterior_prob|bayes_factor|alpha|beta|eta", colnames(res))
+  res <- res[,-w]
+  res <- cbind(res,postm)
+  return(res)
+}
+
+
+
+
+mmseq.lrt <- function(x,e,M,P) {
+  N=length(x)
+  C=ncol(P)
+  V=ncol(M)
+
+  pars=c(1,rep(1,V), rep(1,C-1), rep(1,C))
+
+  op = optim(pars, ll1.func, x=x, e=e, M=M, P=P, method="L-BFGS-B", control=list(fnscale= -1, trace=1), lower=c(rep(-Inf, length(pars)-2),0,0), upper=rep(Inf,length(pars)))
+
+  mle1 <- list(value=exp(op$value),
+    convergence=op$convergence,
+    alpha=op$par[1],
+    beta=op$par[2:(V+1)],
+    gamma=c(0, op$par[(V+2):(V+C)]),
+    sigma2=op$par[(V+C+1):(V+2*C)])
+
+  P <- matrix(1, nrow=N,ncol=1)
+  C=ncol(P)
+  pars=c(1, rep(1,V), rep(1,C-1), 1)
+
+  op = optim(pars, ll.func, x=x, e=e, M=M, P=P, method="L-BFGS-B", control=list(fnscale= -1, trace=1), lower=c(rep(-Inf, length(pars)-1),0), upper=rep(Inf,length(pars)))
+
+  mle0 <- list(value=exp(op$value),
+    convergence=op$convergence,
+    alpha=op$par[1],
+    beta=op$par[2:(V+1)],
+    gamma=c(0),
+    sigma2=op$par[(V+C+1):length(op$par)])
+
+  return(list(stat=mle0$value/mle1$value, mle1=mle1, mle0=mle0))
+}
+
+
+
+
+
+
+ll0 <- function(x,e,M,P) {
+  ll0.func <- function(pars, x=x, e=e, M=M) {
+    N=length(x)
+    V=ncol(M)
+    par.alpha=pars[1]
+    par.beta=pars[2:(V+1)]
+    par.sigma2=pars[V+2]
+    Lambda = diag(1/as.vector(P %*% par.sigma2 + e))
+    y=(x - rep(par.alpha,N) - M %*% par.beta - P %*% par.gamma) 
+    return(-.5 *( rep(1,N) %*% diag(log(1/diag(Lambda))) %*% rep(1,N) + 
+      t(y) %*% Lambda %*% (y)
+    ))
+  }
+
+  pars=c(rep(1,1),rep(1,ncol(M)), rep(1,ncol(P)-1), rep(1,ncol(P)))
+
+  op = optim(pars, ll1.func, x=x, e=e, M=M, P=P, method="L-BFGS-B", control=list(fnscale= -1, trace=1), lower=c(rep(-Inf, length(pars)-2),0,0), upper=rep(Inf,length(pars)))
+
+  N=length(x)
+  C=ncol(P)
+  V=ncol(M)
+  opt <- list(value=op$value,
+    convergence=op$convergence,
+    alpha=op$par[1],
+    beta=op$par[2:(V+1)],
+    gamma=c(0, op$par[(V+2):(V+C)]),
+    sigma2=op$par[(V+C+1):(V+2*C)])
+  return(opt)
+}
+
+
+
+
+
+
+
+
